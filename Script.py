526b548d510de7706ae41204f288941478b9a0b9
WarrenHan <49774051+WarrenHan@users.noreply.github.com>
2020-03-12 22:57:52 -0700

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+
+# Will need to change the filepath to github data
+set1 = pd.read_csv('/Users/warrenhan/Downloads/malaria-death-rates.csv')
+set2 = pd.read_csv('/Users/warrenhan/Downloads/incidence-of-malaria.csv')
+set3 = pd.read_csv('/Users/warrenhan/Downloads/global-malaria-deaths-by-world-region.csv')
+data = set1.merge(set2, left_on = ['Entity', 'Year'], right_on = ['Entity', 'Year'], how='outer')
+data = data.merge(set3, left_on = ['Entity', 'Year'], right_on = ['Entity', 'Year'], how='outer')
