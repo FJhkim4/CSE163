00fd4a8f4db669ba57ba539b39a33286c7afd786
WarrenHan <49774051+WarrenHan@users.noreply.github.com>
2020-03-12 23:40:41 -0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -8,4 +8,7 @@ set2 = pd.read_csv('/Users/warrenhan/Downloads/incidence-of-malaria.csv')
 set3 = pd.read_csv('/Users/warrenhan/Downloads/global-malaria-deaths-by-world-region.csv')
 data = set1.merge(set2, left_on = ['Entity', 'Year'], right_on = ['Entity', 'Year'], how='outer')
 data = data.merge(set3, left_on = ['Entity', 'Year'], right_on = ['Entity', 'Year'], how='outer')
-data.drop(columns=['Code_x', 'Code_y'])
+set2 = set2[(set2['Year'] == 2014) | (set2['Year'] == 2015)]
+set1 = set1[(set1['Year'] == 2014) | (set1['Year'] == 2015)]
+
+data = set1.merge(set2, left_on = ['Entity', 'Year', 'Code'], right_on = ['Entity', 'Year', 'Code'], how='left')
