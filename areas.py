79079c0840347d03dfb299ef8e922de91bc01b68
Anne P Farley <62124121+afarley9@users.noreply.github.com>
2020-03-14 14:53:31 -0700

    sq. mi area 2010 us/global

--- areas.py
+++ areas.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
 
+# I haven't fixed all the flake8 shit yet
 def countries(data):
     data['2010'] = data['2010'] * 0.386102  # converts area to sq miles
     data['AREA_2010'] = data['2010']
-    return data[['Country Name', 'AREA_2010']]
+    data['AREA_YEAR'] = 2010
+    return data[['Country Name', 'AREA_2010', 'AREA_YEAR']]
 
 
 def state(data):
-    data['AREA_2010'] = data['SUBHD0303']  # total land area of states-consistent with country land area
+    data['AREA_2010'] = data['SUBHD0303']  # total land area of states, consistent with country land area
     data['STATE'] = data['GCT_STUB.display-label1']
-    return data.loc[1::, ['STATE', 'AREA_2010']]  # for now, includes US total land area
+    data['AREA_YEAR'] = 2010
+    return data.loc[1::, ['STATE', 'AREA_2010', 'AREA_YEAR']]  # includes US total land area
 
 
 def main():
@@ -21,4 +24,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()  # I haven't fixed all the flake8 shit yet
+    main()
