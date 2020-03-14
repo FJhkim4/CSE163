ce6dbce683071389cb96a4810a94d06ff1c25626
Anne P Farley <62124121+afarley9@users.noreply.github.com>
2020-03-14 02:01:33 -0700

    df of area of state and country in 2010
    work in progress

---
+++ areas.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def countries(data):
+    data['2010'] = data['2010'] * 0.386102  # converts area to sq miles
+    data['AREA_2010'] = data['2010']
+    return data[['Country Name', 'AREA_2010']]
+
+
+def state(data):
+    data['TOT_AREA_2010'] = data['SUBHD0301']
+    data['LAND_AREA_2010'] = data['SUBHD0302']
+    data['STATE'] = data['GCT_STUB.display-label']  # can't get rid of US before state bc the original CSV has them labeled as the same :(
+    return data.loc[1::, ['STATE', 'TOT_AREA_2010', 'LAND_AREA_2010']]
+
+
+def main():
+    countries_area = pd.read_csv('https://raw.githubusercontent.com/WarrenHan/CSE163/master/API_AG.LND.TOTL.K2_DS2_en_csv_v2_822348.csv')
+    state_area = pd.read_csv('https://raw.githubusercontent.com/WarrenHan/CSE163/master/DEC_10_SF1_GCTPH1.US01PR_with_ann.csv')
+    countries(countries_area)
+    state(state_area)
+
+
+if __name__ == '__main__':
+    main()  # I haven't fixed all the flake8 shit yet
