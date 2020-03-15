ed9aed72944d4cd9bb26cff6fbc181f0e3cb2809
Anne P Farley <62124121+afarley9@users.noreply.github.com>
2020-03-15 15:56:51 -0700

    CLASSSSSSSSSS

---
+++ switch.py
@@ -0,0 +1,69 @@
+# Warren Han, Joon Ho Kim , Anne Farley
+# CSE 163, Mentor: Wen Qiu
+# Class to be used to switch US state IDs to their representative names.
+# add(id, name) is so if there isn't a state ID that exists in the dictiomary,
+# it can be added.
+class IdSwitch:
+    def __init__ (self, list):
+        self._dict = {
+            'AL': 'Alabama',
+            'AK': 'Alaska',
+            'AZ': 'Arizona',
+            'AR': 'Arkansas',
+            'CA': 'California',
+            'CO': 'Colorado',
+            'CT': 'Connecticut',
+            'DE': 'Delaware',
+            'FL': 'Florida',
+            'GA': 'Georgia',
+            'HI': 'Hawaii',
+            'ID': 'Idaho',
+            'IL': 'Illinois',
+            'IN': 'Indiana',
+            'IA': 'Iowa',
+            'KS': 'Kansas',
+            'KY': 'Kentucky',
+            'LA': 'Louisiana',
+            'ME': 'Maine',
+            'MD': 'Maryland',
+            'MA': 'Massachusetts',
+            'MI': 'Michigan',
+            'MN': 'Minnesota',
+            'MS': 'Mississippi',
+            'MO': 'Missouri',
+            'MT': 'Montana',
+            'NE': 'Nebraska',
+            'NV': 'Nevada',
+            'NH': 'New Hampshire',
+            'NJ': 'New Jersey',
+            'NM': 'New Mexico',
+            'NY': 'New York',
+            'NC': 'North Carolina',
+            'ND': 'North Dakota',
+            'OH': 'Ohio',
+            'OK': 'Oklahoma',
+            'OR': 'Oregon',
+            'PA': 'Pennsylvania',
+            'RI': 'Rhode Island',
+            'SC': 'South Carolina',
+            'SD': 'South Dakota',
+            'TN': 'Tennessee',
+            'TX': 'Texas',
+            'UT': 'Utah',
+            'VT': 'Vermont',
+            'VA': 'Virginia',
+            'WA': 'Washington',
+            'WV': 'West Virginia',
+            'WI': 'Wisconsin',
+            'WY': 'Wyoming',
+        }
+        self._list = list
+
+    def add(self, id, name):
+        self._dict[id] = name
+    
+    def switch(self):
+        for state in self._list:
+            self._list[self._list.index(state)] = self._dict[state]
+            print(self._list)
+        return self._list
