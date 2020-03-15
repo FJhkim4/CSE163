51c35652e35e602a6b61d9f6206749ead6124001
Anne P Farley <62124121+afarley9@users.noreply.github.com>
2020-03-15 11:48:54 -0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,9 +3,9 @@ import pandas as pd
 import math
 
 # Will need to change the filepath to github data
-set1 = pd.read_csv('/Users/warrenhan/Downloads/malaria-death-rates.csv')
-set2 = pd.read_csv('/Users/warrenhan/Downloads/incidence-of-malaria.csv')
-set3 = pd.read_csv('/Users/warrenhan/Downloads/global-malaria-deaths-by-world-region.csv')
+set1 = pd.read_csv('https://raw.githubusercontent.com/WarrenHan/CSE163/master/malaria-death-rates.csv')
+set2 = pd.read_csv('https://raw.githubusercontent.com/WarrenHan/CSE163/master/incidence-of-malaria.csv')
+set3 = pd.read_csv('https://raw.githubusercontent.com/WarrenHan/CSE163/master/global-malaria-deaths-by-world-region.csv')
 data = set1.merge(set2, left_on = ['Entity', 'Year'], right_on = ['Entity', 'Year'], how='outer')
 data = data.merge(set3, left_on = ['Entity', 'Year'], right_on = ['Entity', 'Year'], how='outer')
 set2 = set2[(set2['Year'] == 2014) | (set2['Year'] == 2015)]
