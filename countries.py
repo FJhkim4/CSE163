530d5943dfc5eda2e91703a74fca3893ecef3017
Anne P Farley <62124121+afarley9@users.noreply.github.com>
2020-03-14 22:42:50 -0700

    up to date country shit

--- countries.py
+++ countries.py
@@ -1,40 +1,75 @@
+# Warren Han, Joon Ho Kim, Anne Farley
+# CSE 163, Mentor: Wen Qiu
+# Descriptiom
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
 
 
-# need to create hosp_desnity, GDP_PER_CAPITA
+# TO DO:
+# names of countries work for merge (e.g. S. Sudan vs South Sudan)
+# find hospital data/hosp_desnity(merge)
+# get malaria data integrated
+# get temp data to work
+
 def area(file):
+    """
+    Description
+    """
     data = pd.read_csv(file)
-    data['2010'] = data['2010'] * 0.386102  # converts area to sq miles
-    data['AREA'] = data['2010']
+    data.rename(columns={'2010': 'AREA', 'Country Name': 'NAME'},
+                inplace=True)  # renames columns
+    data['AREA'] = data['AREA'] * 0.386102  # converts area to sq miles
     data['AREA_YEAR'] = 2010
-    data['NAME'] = data['Country Name']
-    return data[['NAME', 'AREA', 'AREA_YEAR']]
+    return data[['NAME', 'AREA', 'AREA_YEAR']].dropna()
 
 
-def temp(file):  # need to finish
+def temp(file):
+    """
+    need to finish, plus TEMP_YEAR
+    """
     data = pd.read_csv(file)
-    # data = data[data['dt'] is True for 2013 in data['dt']] #idk wtf im doing here, just trying to make something work
+    # data = data[data['dt'] is True for 2013 in data['dt']]
+    return data
 
 
 def shape(file):
+    """
+    Description
+    """
     data = gpd.read_file(file)  # gdp in millions of dollars
-    data = data[['NAME', 'GDP_MD_EST', 'POP_EST', 'GDP_YEAR', 'POP_YEAR', 'CONTINENT', 'geometry']]
+    data = data[['NAME', 'GDP_MD_EST', 'POP_EST', 'GDP_YEAR',
+                 'POP_YEAR', 'CONTINENT', 'geometry']]
     data['GDP_CAPITA'] = data['GDP_MD_EST'] / data['POP_EST']
     return data
 
 
 def hospital(file):
+    """
+    find data, hospital_year,hosp_density
+    """
     data = pd.read_csv(file)
+    return data
 
 
-# I couldn't get all the files to link from the internet...you'll have to download some
+def merge(area, temp, shape, hosp, malaria):
+    """
+    Merges cleaned datasets into mother dataframe for countries
+    """
+    print('WHOOO')
+
+
+# I couldn't get all the files to link from the internet
 def main():
-    area('https://raw.githubusercontent.com/WarrenHan/CSE163/master/API_AG.LND.TOTL.K2_DS2_en_csv_v2_822348.csv')
-    temp('https://raw.githubusercontent.com/WarrenHan/CSE163/master/GlobalLandTemperaturesByCountry.csv')
-    shape('/Users/wopr/Documents/Final Project Anne/test/data/ne_110m_admin_0_countries.shp')
-    # hospital(file) need hospital data
+    d1 = area('https://raw.githubusercontent.com/WarrenHan/CSE163/master' + (
+        '/API_AG.LND.TOTL.K2_DS2_en_csv_v2_822348.csv'))
+    d2 = temp('https://raw.githubusercontent.com/WarrenHan/CSE163/master/' + (
+        'GlobalLandTemperaturesByCountry.csv'))
+    d3 = shape('/Users/wopr/Documents/Final Project Anne/test/data/' + (
+        'ne_110m_admin_0_countries.shp'))
+    # d4 = hospital(file)
+    # d5 = malaria shit--warren??
+    # merge(d1, d2, d3, d4, d5)
+
 
 if __name__ == '__main__':
     main()
