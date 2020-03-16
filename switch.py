c5db15325ac8dd8101ef433989ac6a6c36979e46
Anne P Farley <62124121+afarley9@users.noreply.github.com>
2020-03-15 19:02:50 -0700

    UPDATED CLASS

--- switch.py
+++ switch.py
@@ -1,10 +1,16 @@
 # Warren Han, Joon Ho Kim , Anne Farley
 # CSE 163, Mentor: Wen Qiu
 # Class to be used to switch US state IDs to their representative names.
-# add(id, name) is so if there isn't a state ID that exists in the dictiomary,
-# it can be added.
+# add() allows users to convert additional state IDs into names if not
+# originally provided in the dictionary
+
+
 class IdSwitch:
-    def __init__ (self, list):
+    def __init__(self, states):
+        """
+        Uses dictionary found @ https://gist.github.com/rogerallen/1583593
+        to convert a series of state IDs to their full names
+        """
         self._dict = {
             'AL': 'Alabama',
             'AK': 'Alaska',
@@ -57,13 +63,20 @@ class IdSwitch:
             'WI': 'Wisconsin',
             'WY': 'Wyoming',
         }
-        self._list = list
+        self._states = states
 
     def add(self, id, name):
+        """
+        Allows for user to add a full name for a given ID, if not originally
+        in the provided dictionary.
+        """
         self._dict[id] = name
-    
+
     def switch(self):
-        for state in self._list:
-            self._list[self._list.index(state)] = self._dict[state]
-            print(self._list)
-        return self._list
+        """
+        Uses the above dictionary to switch ID strings into their
+        corresponding state name
+        """
+        for state in self._states:
+            self._states[self._states == state] = self._dict[state]
+        return self._states
